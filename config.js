336be7e72153dd14030b009d79aac4bbccee06ba
yimity <yimity@gmail.com>
2015-12-01 17:12:15 +0800

    增加排除选项，优化逻辑，异常处理，root 自动获取等

--- config.js
+++ config.js
@@ -4,11 +4,13 @@
 'use strict';
 
 module.exports = {
-    port    : 8080,
-    index   : ['index.html', 'default.htm', 'default.html'],
-    root    : 'D:/serve-komplete/public',
-    extend  : ['html', 'htm'],
-    dist    : 'dist',
+    port    : 8080,  // 访问端口 必须
+    index   : ['index.html', 'default.htm', 'default.html'], // 当访问的地址是目录结尾时，默认查找的文件。 可选，默认 'index.html' 和 'default.htm'
+    root    : '', //'D:/serve-komplete/public', // 项目文件所在的目录。可选，默认为 lib 所在目录的 public 目录
+    extend  : ['html', 'htm'],  // 静态文件扩展名 可选 默认 'html', 'htm'
+    exclude : ['layout.html','docs.html'],  // 排除的文件，即访问的时候不生成此文件的完整版本 可选，默认为空
+    dist    : 'dist', // 生成的完整的文件所在的路径 root + dist， 可选 默认为 root + '/dist/'
+    // 渲染引擎的配置，可选，默认为上面的配置
     nunjucks: {
         autoescape      : true,  // (默认值: true) 控制输出是否被转义，查看 Autoescaping
         throwOnUndefined: false, // (default: false) 当输出为 null 或 undefined 会抛出异常
@@ -17,8 +19,15 @@ module.exports = {
         watch           : false, // (默认值: false) 当模板变化时重新加载
         noCache         : true  // (default: false) 不使用缓存，每次都重新编译
     },
+    /*
+     * 渲染模板时候传递的数据，默认为空，可以是纯 json 数据，如果是纯 json 数据，则会将 query 查询参数和 data 合并。
+     * 可以是函数，如果是函数，则会将 query 数据以对象的形式传递进此函数，并在函数执行完毕后，将数据返回给渲染引擎
+     * */
     data : function(data){
         data.title = 'Komplete';
         return data;
     }
+    /*data : {
+        a: 'A'
+    }*/
 };
